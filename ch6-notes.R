52774cf2f98b7ac3a9748e9a9de336a2802bb002
Ilan Reinstein <ilan.reinstein@nyulangone.org>
2019-11-11 16:29:48 -0500

    added chapter 6 files

---
+++ ch6-notes.R
@@ -0,0 +1,16 @@
+library(rethinking)
+
+sppnames <- c("afarensis", "africanus", "habilis", "boisei", "rudolfensis",
+              "ergaster", "sapiens")
+brainvolcc <- c(438, 452, 612, 521, 752, 871, 1350)
+masskg <- c(37.0, 35.5, 34.5, 41.5, 55.5, 61, 53.5)
+d <- data.frame(species = sppnames, brain = brainvolcc, mass = masskg)
+
+# Simplest model possible
+m6.1 <- lm(brain ~ mass, data=d)
+
+# compute R2 
+1 - var(resid(m6.1))/var(d$brain)
+summary(m6.1)
+
+m6.2 <- lm(brain ~ mass + I(mass^2), data=d)
